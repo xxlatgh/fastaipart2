3b0dec086a35a9054424856e19d1470fc2dd2c7a
Xinxin Li <xinxin.li.seattle@gmail.com>
2017-03-10 09:01:44 -0800

    - Updated myutils to include image preprocessing helper functions - added methods to make an image tile with arbitrary number of repeats

--- lesson1/myutils.py
+++ lesson1/myutils.py
@@ -1,15 +1,130 @@
 from PIL import Image
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
+import PIL 
 
 import importlib
-import utils2; importlib.reload(utils2)
-from utils2 import *
 from scipy.optimize import fmin_l_bfgs_b
 from scipy.misc import imsave
 
-from keras import metrics
-from vgg16_avg import VGG16_Avg
+
+def img_resize(img):
+    if (img.size[0]>500) or (img.size[1]>500):
+        factor = (img.size[0]+img.size[1])/1000
+        img = img.resize(np.divide(img.size, factor).astype('int32'))
+    return img
+
+def img_norm(x, rn_mean):
+    return (x - rn_mean)[:, :, :, ::-1]
+
+def img_preproc(path):
+    img = Image.open(path)
+    img = img_resize(img)
+    imagenet_mean = [123.68, 116.779, 103.939]
+    rn_mean = np.array((imagenet_mean), dtype=np.float32)   
+    img_arr = np.array(img)
+    img_arr = np.expand_dims(img_arr, 0)
+    return img_arr, rn_mean
+
+def get_content(content_path):
+    img_arr, rn_mean = img_preproc(content_path)
+    img_arr = img_norm(img_arr, rn_mean)
+    shp = img_arr.shape
+    return img_arr, shp
+
+def get_style(style_path):
+    style_arr, rn_mean = img_preproc(style_path)
+    style_arr = img_norm(style_arr[:,:,:,:3], rn_mean)
+    shp = style_arr.shape
+    return style_arr, shp
+
+def htile_style(style, num, path):
+    imgs = [style for i in range(num)]
+    min_shape = sorted( [(np.sum(i.size), i.size ) for i in imgs])[0][1]
+    imgs_comb = np.hstack( (np.asarray( i.resize(min_shape) ) for i in imgs ) )
+    imgs_comb = PIL.Image.fromarray( imgs_comb)
+    imgs_comb.save(path)
+    return imgs_comb
+
+def vtile_style(style, num, path):
+    imgs = [style for i in range(num)]
+    min_shape = sorted( [(np.sum(i.size), i.size ) for i in imgs])[0][1]
+    imgs_comb = np.vstack( (np.asarray( i.resize(min_shape) ) for i in imgs ) )
+    imgs_comb = PIL.Image.fromarray( imgs_comb)
+    imgs_comb.save(path)
+    return imgs_comb
+
+
+
+class Evaluator(object):
+    def __init__(self, f, shp): self.f, self.shp = f, shp
+
+    def loss(self, x):
+        loss_, self.grad_values = self.f([x.reshape(self.shp)])
+        return loss_.astype(np.float64)
+
+    def grads(self, x): return self.grad_values.flatten().astype(np.float64)
+
+def solve_image(eval_obj, niter, x):
+    deproc = lambda x,s: np.clip(x.reshape(s)[:, :, :, ::-1] + rn_mean, 0, 255)
+    for i in range(niter):
+        x, min_val, info = fmin_l_bfgs_b(eval_obj.loss, x.flatten(),
+                                         fprime=eval_obj.grads, maxfun=20)
+        x = np.clip(x, -127,127)
+        print('Current loss value:', min_val)
+        imsave(f'{result_path}/res_at_iteration_{i}.png', deproc(x.copy(), shp)[0])
+    return x
+
+def gram_matrix(x):
+    # We want each row to be a channel, and the columns to be flattened x,y locations
+    features = K.batch_flatten(K.permute_dimensions(x, (2, 0, 1)))
+    # The dot product of this with its transpose shows the correlation 
+    # between each pair of channels
+    return K.dot(features, K.transpose(features)) / x.get_shape().num_elements()
+
+def style_loss(x, targ): 
+    return metrics.mse(gram_matrix(x), gram_matrix(targ))
+
+def content_model(img_arr):
+    model = VGG16_Avg(include_top = False)
+    layer = model.get_layer('block3_conv2').output
+    layer_model = Model(model.input, layer)
+    target = K.variable(layer_model.predict(img_arr))
+    loss = metrics.mse(layer, targ)
+    grads = K.gradients(loss, model.input)
+    fn = K.function([model.input], [loss]+grads)
+    evaluator = Evaluator(fn)
+    return evaluator
+
+def get_initial():
+    rand_img = lambda shape: np.random.uniform(-2.5, 2.5, shape)/100
+    x = rand_img(shp)
+#    plt.imshow(x[0])
+    return x
+
+def get_content_model():
+    content_result_path = '/home/ubuntu/data/results/cafesociety/contents'
+    content_path = '/Users/xinxin/projectnotes/toddlerartist/content/dortha.jpg'
+    content_model
+    x = get_initial()
+    x = solve_image(evaluator, iterations, x)
+    Image.open(content_result_path + 'results/res_at_iteration_9.png')
+    return
+
+def get_style_model():
+    style_result_path = '/home/ubuntu/data/results/cafesociety/styles'
+    style_path = '/Users/xinxin/projectnotes/toddlerartist/style/flowerplat.jpg'
+    x = get_initial()
+    x = solve_image(evaluator, iterations, x)
+    style_model()#define style_model
+    Image.open(style_result_path + 'results/res_at_iteration_9.png')
+    return
+
+def get_neural_style():
+    merge_result_path = '/home/ubuntu/data/results/miaavery/'
+    #tobecontinue
+
+    
 
 def img_rotate_expand(img, crop_dim):
     newimg = img.rotate(90, expand=1).crop(crop_dim)
@@ -39,83 +154,7 @@ def plots(ims, merge_path, figsize = (12, 6), row=1, cols=None, interp=None, tit
 
     f.savefig(merge_path+'/summary.png') #need to test it
     return
-
-def get_img_arr(img, rn_mean):
-    preproc = lambda x: (x - rn_mean)[:, :, :, ::-1]
-    img_arr = np.array(img)
-    img_arr = np.expand_dims(img_arr, 0)
-    img_arr = preproc(img_arr)
-    return img_arr
-
-#def get_style_arr(style):
-
-
-def get_content(content_path):
-    img = Image.open(content_path)
-    imagenet_mean = [123.68, 116.779, 103.939]
-    rn_mean = np.array((imagenet_mean), dtype=np.float32)
-    img_arr = get_image_arr(img, rn_mean)
-    shp = img_arr.shape
-    return img_arr, shp
-
-class Evaluator(object):
-    def __init__(self, f, shp): self.f, self.shp = f, shp
-
-    def loss(self, x):
-        loss_, self.grad_values = self.f([x.reshape(self.shp)])
-        return loss_.astype(np.float64)
-
-    def grads(self, x): return self.grad_values.flatten().astype(np.float64)
-
-def solve_image(eval_obj, niter, x):
-    deproc = lambda x,s: np.clip(x.reshape(s)[:, :, :, ::-1] + rn_mean, 0, 255)
-    for i in range(niter):
-        x, min_val, info = fmin_l_bfgs_b(eval_obj.loss, x.flatten(),
-                                         fprime=eval_obj.grads, maxfun=20)
-        x = np.clip(x, -127,127)
-        print('Current loss value:', min_val)
-        imsave(f'{result_path}/res_at_iteration_{i}.png', deproc(x.copy(), shp)[0])
-    return x
-
-def content_model(img_arr):
-    model = VGG16_Avg(include_top = False)
-    layer = model.get_layer('block3_conv2').output
-    layer_model = Model(model.input, layer)
-    target = K.variable(layer_model.predict(img_arr))
-    loss = metrics.mse(layer, targ)
-    grads = K.gradients(loss, model.input)
-    fn = K.function([model.input], [loss]+grads)
-    evaluator = Evaluator(fn)
-    return evaluator
-
-def get_initial():
-    rand_img = lambda shape: np.random.uniform(-2.5, 2.5, shape)/100
-    x = rand_img(shp)
-#    plt.imshow(x[0])
-    return x
-
-def get_content():
-    content_result_path = '/home/ubuntu/data/results/cafesociety/contents'
-    content_path = '/Users/xinxin/projectnotes/toddlerartist/content/dortha.jpg'
-    content_model
-    x = get_initial()
-    x = solve_image(evaluator, iterations, x)
-    Image.open(content_result_path + 'results/res_at_iteration_9.png')
-    return
-
-def get_style():
-    style_result_path = '/home/ubuntu/data/results/cafesociety/styles'
-    style_path = '/Users/xinxin/projectnotes/toddlerartist/style/flowerplat.jpg'
-    x = get_initial()
-    x = solve_image(evaluator, iterations, x)
-    style_model()#define style_model
-    Image.open(style_result_path + 'results/res_at_iteration_9.png')
-    return
-
-def get_neural_style():
-    merge_result_path = '/home/ubuntu/data/results/miaavery/'
-    #tobecontinue
-
+    
 '''
 def main():
     content_path = ''
